72a658120ef988eda2d478f239836c31040fcd5b
danielcinome <danielchinome987@gmail.com>
2020-03-11 17:56:44 +0000

    [Task 9-13] Correction

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL | tree->right != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 	{
 		val = binary_tree_nodes(tree->left);
 		val1 = binary_tree_nodes(tree->right) + 1;
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t val = 0;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree != NULL)
 	{
 		if (tree->left != NULL)

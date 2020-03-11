7da9f5f8cef30c717eece2aacccaaad60657d65a
danielcinome <danielchinome987@gmail.com>
2020-03-11 20:53:41 +0000

    [Task 16] Correction

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,7 +10,7 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int val_l = 0, val_r = 0, h_left = 0, h_right = 0;
+	int val_l = 0, val_r = 0, b_tree = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -20,11 +20,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree->left)
 		val_l = binary_tree_is_full(tree->left);
-		h_left = binary_height(tree->left);
 	if (tree->right)
 		val_r = binary_tree_is_full(tree->right);
-		h_right = binary_height(tree->right);
-	if (h_left == h_right && val_l == 1 && val_r == 1)
+	b_tree = binary_tree_balance(tree);
+	if ((b_tree == 0) && (val_l == 1 && val_r == 1))
 		return (1);
 	else
 		return (0);

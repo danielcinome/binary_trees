7a05c5cda1fc341d131964940f5ccc33f4b4b4ee
danielcinome <danielchinome987@gmail.com>
2020-03-11 17:42:27 +0000

    [Task 6..9] Correction and deleted double prototypes in file .h

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree->left != NULL)
 			val = binary_tree_height(tree->left) + 1;
-		if (tree->right)
+		if (tree->right != NULL)
 			val = binary_tree_height(tree->right) + 1;
 	}
 	return (val);

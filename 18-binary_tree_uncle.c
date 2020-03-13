fe27bbeec3547b4512f3dff13d5a5bfc1ce4f0ef
Andresmelek <candres.isaza@gmail.com>
2020-03-13 13:40:44 -0500

    Fixing task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+#include "17-binary_tree_sibling.c"
 /**
  *  binary_tree_uncle - function that finds the uncle of a node
  *  @node:  is a pointer to the node to find the uncle
@@ -9,42 +9,10 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	int val = 0;
+	binary_tree_t *uncle;
 
-	if (node == NULL || node->parent == NULL)
+	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->left != NULL && node->parent->right != NULL)
-	{
-		if (node->parent->parent == NULL)
-			return (NULL);
-		if (node->parent->left == node)
-		{
-			while (node->parent != NULL)
-			{
-				node = node->parent;
-				val++;
-			}
-			while (val > 1)
-			{
-				node = node->left;
-				val--;
-			}
-			return (node);
-		}
-		else
-		{
-			while (node->parent != NULL)
-			{
-				node = node->parent;
-				val++;
-			}
-			while (val > 1)
-			{
-				node = node->right;
-				val--;
-			}
-			return (node);
-		}
-	}
-	return (0);
+	uncle = binary_tree_sibling(node->parent);
+	return (uncle);
 }
